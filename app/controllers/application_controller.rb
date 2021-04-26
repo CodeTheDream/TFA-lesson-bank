b3f8a78f9ff4669fa23edb6e608b92bc42040199
chuck <charlesvincentanderson@gmail.com>
2021-04-26 14:49:55 -0400

    implemented devise for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:role, :email, :password, :password_confirmation, :unconfirmed_email]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+  end
+
+  def configure_sign_up_parameters
+    params.require(:user).permit(:email, :role, :password, :password_confirmation, :unconfirmed_email)
+  end
 end
