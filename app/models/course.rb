7938d0c08be96875227cfc97961bbc9a410bce28
Adriana Cabrera <adi_111@icloud.com>
2021-04-29 22:18:48 -0700

    associations on course and lessons

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
+    validates :grade_level, numericality: { only_integer: true }
+    # ARE WE GOING TO CHECK LENGHTH Y/N
+    # validates :grade_level, length: { is: 10}
     belongs_to :user
+    has_many :lessons, through: :courses 
 end
