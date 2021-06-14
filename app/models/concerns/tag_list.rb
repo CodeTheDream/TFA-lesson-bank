5a3429fbcca3581713a0d08ca813c91ba045b363
Chuck <charlesvincentanderson@gmail.com>
2021-06-14 13:49:44 -0400

    added comments

--- app/models/concerns/tag_list.rb
+++ app/models/concerns/tag_list.rb
@@ -6,9 +6,10 @@ module TagList
   end
 
   def tag_list=(input_tag_names)
-    # for update
+    # find out what kind of object owns these tags
     is_course = self.kind_of?(Course)
     object = self
+    # we need to dereference or delete tags in a way that updating tags does not effect other courses or lessons
     if self.tags.any?
       self.tags.each do |tag|
 	key_word = is_course ? KeyWord.find_by(course_id: object.id, tag_id: tag.id) : KeyWord.find_by(lesson_id: object.id, tag_id: tag.id)
@@ -17,7 +18,7 @@ module TagList
         (tag.lessons.count + tag.courses.count) > 1 ? key_word.delete : tag.delete
       end
     end
-    # for create and update
+    # create tags and/or associations through key_words for all values passed in
     tag_names = input_tag_names.collect{|t| t.strip.downcase}.uniq
     tag_names = tag_names.reject {|x| x == ""}
     new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
