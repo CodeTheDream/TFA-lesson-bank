6e3afd76aa5488ab17ff660fe9e3156e6ef89208
Adriana Cabrera <adi_111@icloud.com>
2021-06-29 00:16:02 -0700

    more specs for lessons

--- spec/requests/lessons_spec.rb
+++ spec/requests/lessons_spec.rb
@@ -27,33 +27,47 @@ RSpec.describe "Lessons", type: :request do
     end
   end
 
-describe "get show_assignment_path" do
-  # doesn't render the :show template if a user is not logged in' ???
-  it "get show_assignment_path" do
-    @user = FactoryBot.create(:user)
-    sign_in @user
-    @user.confirm
-    course_hash = {title: "React", description: "React", subject: "Hooks", grade_level: 2, state: "NC", district: "02", start_date: "2021-05-05 00:00:00", end_date: "2021-12-31 00:00:00", created_at: Time.now, updated_at: Time.now, user_id: @user.id} 
-    @course = Course.create(course_hash)
-    lesson_hash = {title: "test lesson1", description: "test lesson1", 
-      created_at: Time.now, updated_at: Time.now, units_covered: "3",  course_id: @course.id}
-    @lesson = Lesson.create(lesson_hash)
-    get course_lesson_path(course_id: @course.id, id: @lesson.id)
-    expect(response).to render_template(:show)
+  describe "get show_assignment_path" do
+    # doesn't render the :show template if a user is not logged in' ???
+    it "get show_assignment_path" do
+      @user = FactoryBot.create(:user)
+      sign_in @user
+      @user.confirm
+      course_hash = {title: "React", description: "React", subject: "Hooks", grade_level: 2, state: "NC", district: "02", start_date: "2021-05-05 00:00:00", end_date: "2021-12-31 00:00:00", created_at: Time.now, updated_at: Time.now, user_id: @user.id} 
+      @course = Course.create(course_hash)
+      lesson_hash = {title: "test lesson1", description: "test lesson1", 
+        created_at: Time.now, updated_at: Time.now, units_covered: "3",  course_id: @course.id}
+      @lesson = Lesson.create(lesson_hash)
+      get course_lesson_path(course_id: @course.id, id: @lesson.id)
+      expect(response).to render_template(:show)
+    end
+    
+    it "redirects to the courses index path if the lesson id is invalid" do
+      @user = FactoryBot.create(:user)
+      sign_in @user
+      @user.confirm
+      course_hash = {title: "React", description: "React", subject: "Hooks", grade_level: 2, state: "NC", district: "02", start_date: "2021-05-05 00:00:00", end_date: "2021-12-31 00:00:00", created_at: Time.now, updated_at: Time.now, user_id: @user.id} 
+      @course = Course.create(course_hash)
+      lesson_hash = {title: "test lesson1", description: "test lesson1", 
+        created_at: Time.now, updated_at: Time.now, units_covered: "3",  course_id: @course.id}
+      @lesson = Lesson.create(lesson_hash)
+      get course_lesson_path(course_id: @course.id, id: 5000)
+      expect(response).to redirect_to courses_path
+    end
   end
-  
-  it "redirects to the courses index path if the lesson id is invalid" do
-    @user = FactoryBot.create(:user)
-    sign_in @user
-    @user.confirm
-    course_hash = {title: "React", description: "React", subject: "Hooks", grade_level: 2, state: "NC", district: "02", start_date: "2021-05-05 00:00:00", end_date: "2021-12-31 00:00:00", created_at: Time.now, updated_at: Time.now, user_id: @user.id} 
-    @course = Course.create(course_hash)
-    lesson_hash = {title: "test lesson1", description: "test lesson1", 
-      created_at: Time.now, updated_at: Time.now, units_covered: "3",  course_id: @course.id}
-    @lesson = Lesson.create(lesson_hash)
-    get course_lesson_path(course_id: @course.id, id: 5000)
-    expect(response).to redirect_to courses_path
+  describe "get new_course_lesson_path" do
+    it "renders the :new template" do
+      @user = FactoryBot.create(:user)
+      sign_in @user
+      @user.confirm
+      course_hash = {title: "React", description: "React", subject: "Hooks", grade_level: 2, state: "NC", district: "02", start_date: "2021-05-05 00:00:00", end_date: "2021-12-31 00:00:00", created_at: Time.now, updated_at: Time.now, user_id: @user.id} 
+      @course = Course.create(course_hash)
+      lesson_hash = {title: "test lesson1", description: "test lesson1", 
+        created_at: Time.now, updated_at: Time.now, units_covered: "3",  course_id: @course.id}
+      @lesson = Lesson.create(lesson_hash)
+      get new_course_lesson_path(course_id: @course.id, id: @lesson.id),
+        params: {lesson: {title: "React", description: "React", subject: "Hooks", grade_level: 2, state: "NC", district: "02", start_date: "2021-05-05 00:00:00", end_date: "2021-12-31 00:00:00", created_at: Time.now, updated_at: Time.now, user_id: @user.id}}
+      expect(response).to render_template(:new)
+    end
   end
-end
-
 end
