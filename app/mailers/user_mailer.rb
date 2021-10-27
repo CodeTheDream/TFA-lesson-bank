b82d17ad758bdc5ea25f894fad5aab683f0188e6
Adriana Cabrera <adi_111@icloud.com>
2021-10-26 17:52:24 -0700

    Removing some extra spaces

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,7 +14,6 @@ class UserMailer < ApplicationMailer
       from: "Teach for America team",
       to: @user.email,
       subject: "New User Signup: #{@user.email}")
-      # subject: 'Welcome to Teach for America')
   end
   def update_user
     @user = params[:user]
@@ -24,7 +23,7 @@ class UserMailer < ApplicationMailer
       to: @user.email,
       subject: "Update User: #{@user.email}") 
   end
-
+end
   # do |format|
   #   format.html { render 'another_template' }
   #   format.text { render plain: 'Render text' }
@@ -36,5 +35,5 @@ class UserMailer < ApplicationMailer
   #   # @user = User.find_by_email(params[:email])
   #   UserMailer.password_reset.deliver
   # end
-end
+
 
