aa21dcd5567fbd2247fe3cd0131361ac2305e918
Chuck <charlesvincentanderson@gmail.com>
2021-11-30 17:16:08 -0500

    searchable by last name

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -57,7 +57,7 @@ class CoursesController < ApplicationController
     if @course.save
       @course.tag_list=(tags_params.values) if params[:tag_names].present?
       course_tags = @course.tags.pluck(:name).join(' ')
-      hash = { searchable_id: @course.id, searchable_type: 'Course', title: @course.title, description: @course.description, subject: @course.subject, state: @course.state, district: @course.district, grade_level: @course.grades.pluck(:grade_level).join(' '), tags: course_tags, user_id: current_user.id } 
+      hash = { searchable_id: @course.id, searchable_type: 'Course', title: @course.title, description: @course.description, subject: @course.subject, state: @course.state, district: @course.district, grade_level: @course.grades.pluck(:grade_level).join(' '), tags: course_tags, user_id: current_user.id, last_name: current_user.last_name } 
       search_item = SearchItem.create(hash)
       @course.search_item = search_item
       flash.notice = "The course record was created successfully."
@@ -88,7 +88,7 @@ class CoursesController < ApplicationController
       end
       @course.tag_list=(tags) if tags.present?
       course_tags = @course.tags.pluck(:name).join(' ')
-      hash = { searchable_id: @course.id, searchable_type: 'Course', title: @course.title, description: @course.description, subject: @course.subject, state: @course.state, district: @course.district, grade_level: @course.grades.pluck(:grade_level).join(' '), tags: course_tags, user_id: current_user.id } 
+      hash = { searchable_id: @course.id, searchable_type: 'Course', title: @course.title, description: @course.description, subject: @course.subject, state: @course.state, district: @course.district, grade_level: @course.grades.pluck(:grade_level).join(' '), tags: course_tags, user_id: current_user.id, last_name: current_user.last_name } 
       search_item = SearchItem.find_by(searchable_id: @course.id, searchable_type: 'Course')
       search_item.update hash
       @course.search_item = search_item
