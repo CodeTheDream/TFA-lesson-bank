b92f80c592cd672e2d2007b2177a73b0dd2b0734
Adriana Cabrera <adi_111@icloud.com>
2022-01-04 19:31:07 -0800

    Validates uniqueness in the log table when the user download the files

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,8 @@
 class Log < ApplicationRecord
   belongs_to :user
+  #User can save one record on the log when the user download the file 
+  validates :user_id, uniqueness: { 
+    scope: [:user_id, :document_id],
+    message: 'User can only save the log one time'
+  }
 end 
