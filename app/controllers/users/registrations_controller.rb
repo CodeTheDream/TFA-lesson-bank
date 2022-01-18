1f47dcc0ae4729be50579ff77d98bc90a9c922ba
Adriana Cabrera <adi_111@icloud.com>
2022-01-18 00:20:27 -0800

    Last change to get the buttons to download cvs and excel

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -39,7 +39,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       respond_to do |format|
         format.html
         format.csv { send_data @logs.to_csv }
-        format.xls #{ send_data @logs.to_csv(col_sep: "\t") }
+        format.xls { send_data @logs.to_csv(col_sep: "\t") }
       end
   end
 
@@ -54,7 +54,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       respond_to do |format|
         format.html
         format.csv { send_data @logs.to_csv }
-        format.xls #{ send_data @logs.to_csv(col_sep: "\t") }
+        format.xls { send_data @logs.to_csv(col_sep: "\t") }
       end
   end
 
