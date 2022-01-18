dd14cf4b42da53643df18d41aa07f333517cd2b7
Adriana Cabrera <adi_111@icloud.com>
2022-01-18 00:29:03 -0800

    Last change to get the buttons to download cvs and excel and clean some code

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -24,7 +24,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
   
   def userlogs
-    # @logs = @user.logs 
       @logs = Log.order(:id)
       # Documents downloaded by this user
       @documentsid = Log.where(creator_id: @user.id).distinct.pluck :document_id
@@ -32,10 +31,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       @documentsid.each do |creator|
         @creatordoc = Document.find(creator).lesson_id.present? ? Document.find(creator).lesson.course.user : Document.find(creator).course.user
       end
-      # # @creatordoccursos = Document.find(2).course.user
-      # @creatordoc = Document.find(2).lesson_id.present? ? Document.find(2).lesson.course.user : Document.find(2).course.user
-      # @creatordoc = Document.find(7).lesson_id.present? ? Document.find(7).lesson.course.user : Document.find(7).course.user
-
+ 
       respond_to do |format|
         format.html
         format.csv { send_data @logs.to_csv }
