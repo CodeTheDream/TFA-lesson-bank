a19d5453424d2b3d9bb6432592dc7fdbffbdf6b7
sheilahd <sheila.hdez@gmail.com>
2022-03-21 13:04:25 -0400

    adding responsiveness and refactoring CourseLessonForm Edit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,21 +13,14 @@ class PagesController < ApplicationController
     @search = query
     @selected_subject  = search_params[:subject].present? ? search_params[:subject] : ''
     @selected_district  = search_params[:district].present? ? search_params[:district] : ''
-    @selected_grades = nil
-    if search_params[:selected_grades].present?
-      @selected_grades = search_params[:selected_grades].keys 
-    elsif search_params[:selected_grades_back].present?
-      @selected_grades = eval(search_params[:selected_grades_back])
-    end
-      
-    
+    @selected_grades = search_params[:available_grade_levels].present? ? search_params[:available_grade_levels] : {}
     @selected_types = []
     @selected_types << "courses" if params[:courses] == "true"
     @selected_types << "lessons" if params[:lessons] == "true"
     @results = SearchItemSearch.search(query: query, options: search_params, current_user: current_user)
-
-    if @selected_grades.present?
-      @results =  @results.select {|x| (x.grade_level.split(' ') - (@selected_grades)) != x.grade_level.split(' ')}
+  
+    if search_params[:available_grade_levels].present?
+      @results =  @results.select {|x| (x.grade_level.split(' ') - (search_params[:available_grade_levels].keys)) != x.grade_level.split(' ')}
     end
     if search_params[:favorites] == "true"
       favorites = Favorite.where(user_id: current_user.id)
