28ebc7bd6aba75624131120b2efe953b490303e8
sheilahd <sheila.hdez@gmail.com>
2022-03-22 09:30:13 -0400

    adding merge conflict changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -55,10 +55,6 @@ class PagesController < ApplicationController
   private
 
   def search_params
-<<<<<<< HEAD
     params.permit(:commit, :search, :page, :sort_attribute, :sort_order, :title, :description, :subject, :state, :district, :favorites, :mycontent, :courses, :lessons, :selected_grades_back, :selected_grades => {} )
-=======
-    params.permit(:commit, :search, :page, :sort_attribute, :sort_order, :title, :description, :subject, :state, :district, :favorites, :mycontent, :courses, :lessons, :flags, :available_grade_levels => {} )
->>>>>>> development
   end
 end
