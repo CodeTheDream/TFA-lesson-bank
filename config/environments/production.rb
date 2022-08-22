7396a3583b87f5f11d7ad1d9f7d8ac2711c0d809
Chuck <charlesvincentanderson@gmail.com>
2022-08-22 13:34:09 -0400

    changed email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "tfa_lesson_bank_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => "tfa-lesson-bank-2nhhr.ondigitalocean.app" }
+  config.action_mailer.default_url_options = { :host => "tfalessonplanbank.onegoalnc.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -123,6 +123,6 @@ Rails.application.configure do
     password: ENV['GMAIL_PASSWORD'],
     authentication: :plain,
     enable_starttls_auto: true,
-    domain: 'fa-lesson-bank-2nhhr.ondigitalocean.app',
+    domain: 'tfalessonplanbank.onegoalnc.com',
   }
 end
